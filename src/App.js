b3a3c6af9b1e139da5799ec97570dc915f8aee16
Keongy <info.pichon@gmail.com>
2021-01-05 13:01:51 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import TemperatureInput from './TemperatureInput';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="row justify-content-center mt-5">
+        <div className="col-3 border">
+          <TemperatureInput />
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/TemperatureInput.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+class TemperatureInput extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return (
+            <div className="form-group">
+                <input type="text" className="form-control"/>
+            </div>
+        )
+    }
+
+}
+
+
+export default TemperatureInput
